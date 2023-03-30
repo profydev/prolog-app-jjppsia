ad0c00d3247e1a3589b68a223c3890b669136c4e
Jp Sia <johnpaulsia4@gmail.com>
2023-03-30 14:10:14 +0800

    Fix navigation support button

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -190,11 +190,12 @@ export function SidebarNavigation() {
           </LinkList>
 
           <List>
-            <MenuItemButton
+            <MenuItemLink
               text="Support"
+              href="mailto:support@prolog-app.com?subject=Support Request:"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              isActive={false}
             />
             <CollapseMenuItem
               text="Collapse"
